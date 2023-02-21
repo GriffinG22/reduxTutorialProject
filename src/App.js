9ce0811a8569330575f427caae2bc17a25a019cb
Griffin Gradke <griffin.gradke@gmail.com>
2023-02-21 16:59:41 -0600

    conditionally render login modal / profile (ND-M)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
+
 import Counter from './components/Counter';
 import Auth from './components/Auth';
 import Header from './components/Header';
+import UserProfile from './components/UserProfile';
 
 function App() {
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+
   return (
     <Fragment>
       <Header />
-      <Auth />
+      {!isAuthenticated ? <Auth /> : <UserProfile />}
       <Counter />
     </Fragment>
   );
