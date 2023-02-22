a0eb56b60c8dce9ee041d4a586c19a72f9d3871c
Griffin Gradke <griffin.gradke@gmail.com>
2023-02-22 10:36:07 -0600

    login/ logout render functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import { authActions } from '../store';
+
 import classes from './Header.module.css';
 
 const Header = () => {
-  return (
-    <header className={classes.header}>
-      <h1>Redux Auth</h1>
-      <nav>
-        <ul>
+
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+
+  const dispatch = useDispatch();
+
+  const logoutHandler = () => {
+    dispatch(authActions.logout());
+  }
+
+  const loggedInNavContent = (
+      <ul>
           <li>
             <a href='/'>My Products</a>
           </li>
@@ -13,9 +23,26 @@ const Header = () => {
             <a href='/'>My Sales</a>
           </li>
           <li>
-            <button>Logout</button>
+            <button onClick={logoutHandler}>Logout</button>
           </li>
-        </ul>
+      </ul>
+  )
+
+  const loggedOutNavContent = (
+    <ul>
+      <li>
+        <button>Login</button>
+      </li>
+    </ul>
+  )
+
+  const navLinkContent = isAuthenticated ? loggedInNavContent : loggedOutNavContent;
+
+  return (
+    <header className={classes.header}>
+      <h1>Redux Auth</h1>
+      <nav>
+        {navLinkContent}
       </nav>
     </header>
   );
